4a013eb8b76cc5d0273d0a5e09457618d84e5368
Kosta <68782786+KostaD02@users.noreply.github.com>
2023-09-30 19:32:03 +0400

    fix: api property display in swagger (#64)

--- src/modules/quote/dtos/update-quote.dto.ts
+++ src/modules/quote/dtos/update-quote.dto.ts
@@ -1,12 +1,19 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsOptional, IsString } from 'class-validator';
 import { QuoteExpectionKeys } from 'src/enums';
 
 export class UpdateQuoteDto {
   @IsOptional()
+  @ApiProperty({
+    required: false,
+  })
   @IsString({ message: QuoteExpectionKeys.AuthorNameShouldBeString })
   author: string;
 
   @IsOptional()
+  @ApiProperty({
+    required: false,
+  })
   @IsString({ message: QuoteExpectionKeys.QuoteShouldBeString })
   quote: string;
 }
