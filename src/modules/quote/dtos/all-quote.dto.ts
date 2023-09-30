a6bf577171e9556547c222d50eca05bd3d55331a
Kosta <68782786+KostaD02@users.noreply.github.com>
2023-09-30 19:31:27 +0400

    fix: api property display in swagger

--- src/modules/quote/dtos/all-quote.dto.ts
+++ src/modules/quote/dtos/all-quote.dto.ts
@@ -1,13 +1,20 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsOptional, IsString } from 'class-validator';
 import { QuoteExpectionKeys } from 'src/enums';
 import { PaginationQueryDto } from 'src/shared';
 
 export class AllQuoteDto extends PaginationQueryDto {
   @IsOptional()
+  @ApiProperty({
+    required: false,
+  })
   @IsString({ message: QuoteExpectionKeys.AuthorNameShouldBeString })
   author: string;
 
   @IsOptional()
+  @ApiProperty({
+    required: false,
+  })
   @IsString({ message: QuoteExpectionKeys.QuoteKeywordShouldBeString })
   keywords: string;
 }
