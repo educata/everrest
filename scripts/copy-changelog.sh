2cdef37bd9efbf8a329c91d14ddf3bbf3a26a929
Kosta <68782786+KostaD02@users.noreply.github.com>
2023-10-05 15:06:32 +0400

    fix: changelog doc (#72)
    * chore: added script copy-changelog
    
    * fix: changed file name for case insensitive

--- scripts/copy-changelog.sh
+++ scripts/copy-changelog.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 if [ "$(uname -s)" == "MINGW64_NT-10.0" ]; then
-    copy changelog.md docs/
+    # windows
+    copy CHANGELOG.md docs/
 else
-    cp changelog.md docs/
+    cp CHANGELOG.md docs/
 fi
