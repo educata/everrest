7f35ad891604a27e1de4866b0646cfc785156140
Kosta <68782786+KostaD02@users.noreply.github.com>
2023-10-25 17:52:51 +0400

    fix: add dto & adjusted link

--- src/modules/echo/echo.controller.ts
+++ src/modules/echo/echo.controller.ts
@@ -2,19 +2,23 @@ import { Body, Controller, Post, Render } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 import { EchoService } from './echo.service';
 
+export class EchoDto {
+  [key: string]: string;
+}
+
 @ApiTags('echo')
 @Controller('echo')
 export class EchoController {
   constructor(private echoService: EchoService) {}
 
   @Post('json')
-  getJson(@Body() body: any) {
+  getJson(@Body() body: EchoDto) {
     return body;
   }
 
   @Post('html')
   @Render('echo')
-  getHtml(@Body() body: object) {
+  getHtml(@Body() body: EchoDto) {
     const text = this.echoService.jsonToHtml(body);
     return { body: text };
   }
